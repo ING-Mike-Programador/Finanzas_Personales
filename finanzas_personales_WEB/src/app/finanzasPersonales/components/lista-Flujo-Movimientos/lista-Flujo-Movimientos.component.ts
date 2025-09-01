61ec012fc577d560521ce3b4f75ffb389c85064b
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2025-08-31 22:25:20 -0700

    optimización de la tabla de flujo y carga de datos del mes actual

--- finanzas_personales_WEB/src/app/finanzasPersonales/components/lista-Flujo-Movimientos/lista-Flujo-Movimientos.component.ts
+++ finanzas_personales_WEB/src/app/finanzasPersonales/components/lista-Flujo-Movimientos/lista-Flujo-Movimientos.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { manejoflujoService } from '../../services/manejo-flujo.service';
 
 @Component({
@@ -7,5 +7,39 @@ import { manejoflujoService } from '../../services/manejo-flujo.service';
   templateUrl: './lista-Flujo-Movimientos.component.html',
 })
 export class ListaFlujoMovimientosComponent {
+  // injeccion de servicio
   flujoService = inject(manejoflujoService);
+
+  // meses del año
+  meses: string[] = [
+    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+  ];
+  // Obtener mes actual
+  mes_Actual = signal<number>(new Date().getMonth() + 1)
+  // Obtener el número de días del mes actual
+  dias_Mes_Actual = signal<number>(new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate());
+  // Obtener el día actual del mes
+  dia_Actual = signal<number>(new Date().getDate());
+
+  // Lista de flujo del mes
+  lista_Flujo_Mes = signal<FlujoMovimientos[]>([]);
+
+  // Al iniciar el componente, cargar la lista de flujo del mes actual
+  ngOnInit() {
+    this.cargar_Lista_Flujo_Mes(this.mes_Actual());
+  }
+
+  // Función para cargar la los dias de flujo del mes
+  cargar_Lista_Flujo_Mes(mes: number) {
+    for (let dia = 1; dia <= this.dias_Mes_Actual(); dia++) {
+      const flujo_Dia: FlujoMovimientos = {
+        fecha: `${dia}-${this.meses[mes - 1]}`,
+        ganancias: 0,
+        gastos: 0,
+        total: 0
+      };
+      this.lista_Flujo_Mes.update(lista => [...lista, flujo_Dia]);
+    }
+  }
  }
--- finanzas_personales_WEB/src/app/finanzasPersonales/services/manejo-flujo.service.ts
+++ finanzas_personales_WEB/src/app/finanzasPersonales/services/manejo-flujo.service.ts
@@ -1,16 +1,7 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class manejoflujoService {
 
-  // Obtener mes actual
-  mes_Actual = signal<number>(new Date().getMonth() + 1)
-  // Obtener el número de días del mes actual
-  dias_Mes_Actual = signal<number>(new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate());
-  // Obtener el día actual del mes
-  dia_Actual = signal<number>(new Date().getDate());
 
-
-  // Lista de flujo del mes
-  lista_Flujo_Mes = signal<FlujoMovimientos[]>([]);
 }
